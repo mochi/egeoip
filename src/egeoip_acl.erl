ececbc34814be90c8d4a7857c19e4f503990283e
Dmitry Demeshchuk <demeshchuk@gmail.com>
2010-11-08 11:24:26 +0300

    New version

--- src/egeoip_acl.erl
+++ src/egeoip_acl.erl
@@ -1,100 +1,248 @@
 -module(egeoip_acl).
 -export([
-    ip_by_location/4,
-    parse_file/1
+    parse_file/1,
+    lookup/2
 ]).
 
-parse_file(Filename) ->
-    {ok, File} = file:open(Filename, [read, binary]),
-    {IPs, LocNames} = process_file(File, [], [], []),
-    {LocationsTree, _} = lists:foldl(
-                        fun(Loc, {Acc, Num}) ->
-                            {gb_trees:insert(Loc, Num, Acc), Num + 1}
-                        end,
-                        {gb_trees:empty(), 0}, LocNames),
-    file:close(File),
-    {BinaryIPs, Offsets} = ips_to_binary(IPs),
-    {BinaryIPs, Offsets, LocationsTree}.
+%% @type ip24() = list() | binary() | {int(), int(), int()}
 
-process_file(File, Locations, CurLoc, LocNames) ->
+%% @spec parse_file(string() | atom()) -> {binary(), binary(), tuple()}
+%% @doc Parses an ACL file and converts it to an IP -> Zone index.
+%%
+%%  IPs DATABASE FORMAT
+%%
+%%  {IPsBlob, IPsIndex, Zones}
+%%
+%%  1. IPsBlob consists of buckets, each bucket is associated with some
+%%     16-bit IP (M.N.*.*) and contains IP ranges associated with a zone hash:
+%%
+%% <<StartIP_1:8, EndIP_1:8, Zone_1:16, StartIP_2:8, EndIP_2:8, Zone_2:16, ...>>
+%%
+%%      Thus, every IP range uses 4 bytes: 2 bytes for start and end of
+%%      the range and 2 bytes for zone hash.
+%%
+%%  2. IPsIndex has 65536 parts associated with all possible 16-bit IPs
+%%     (from 0.0.*.* to 255.255.*.*). Every part has 4 bytes:
+%%
+%%  <<BucketStart:16, BucketSize:16>>
+%%
+%%      BucketStart represents the offset of the bucket from PIsBlob associated
+%%      with the target 16-bit IP, and BucketSize represents its size, in bytes.
+%%
+%%  3. Zones is a simple tuple of zones, associating zone number from IPsBlob's
+%%     buckets with zone name.
+%%
+parse_file(Filename) ->
+    RawLines = read_file(Filename),
+    IPsByZone = parse_line(RawLines, []),
+    {Zones, ZonesTree} = zones_index(IPsByZone),
+    IPZoneKV = extract_ips(IPsByZone, ZonesTree),
+    {IPsBlob, IPsIndex} = ips_index(IPZoneKV),
+    {IPsBlob, IPsIndex, Zones}.
+
+%% @spec lookup(ip24()) -> binary() | notfound
+%% @doc Searches for 24-bit IP in the database generated from ACL file. Returns
+%%      a binary name of the zone or notfound if there's no zone associated with
+%%      the IP.
+%%
+%%      Possible IP formats:
+%%          {127,0,0}
+%%          <<"127.0.0">>
+%%          "127.0.0"
+%%          <<127,0,0>>
+%%
+lookup(IP, Database) when is_list(IP) ->
+    lookup(parse_ip(list_to_binary(IP)), Database);
+lookup({A, B, C}, Database) ->
+    lookup(<<A:8, B:8, C:8>>, Database);
+lookup(<<Prefix:16/integer, C:8>>, {IPsBlob, IPsIndex, Zones}) ->
+    IndexOffset = Prefix * 4,
+    <<_:(IndexOffset)/binary, BucketOffset:16/integer, BucketSize:16/integer, _/binary>> = IPsIndex,
+    <<_:(BucketOffset)/binary, Ranges:(BucketSize)/binary, _/binary>> = IPsBlob,
+    case lookup_ip(C, Ranges) of
+        V when is_integer(V) ->
+            element(V, Zones);
+        _ ->
+            notfound
+    end;
+lookup(IP, Database) when is_binary(IP) ->
+    lookup(parse_ip(IP), Database).
+
+lookup_ip(C, <<Start:8, End:8, ZNum:16/integer, _/binary>>)
+                                                when C >= Start, C =< End ->
+    ZNum;
+lookup_ip(C, <<_:4/binary, Rest/binary>>) ->
+    lookup_ip(C, Rest);
+lookup_ip(_, <<>>) ->
+    notfound.
+
+read_file(Filename) ->
+    {ok, File} = file:open(Filename, [binary, read]),
+    RawLines = read_line(File, []),
+    file:close(File),
+    RawLines.
+
+read_line(File, Acc) ->
     case file:read_line(File) of
-        {ok, <<"acl ", Rest/binary>>} ->
-            process_file(File, Locations, [], [location(Rest) | LocNames]);
-        {ok, <<"};", _Rest/binary>>} ->
-            process_file(File, [CurLoc | Locations], [], LocNames);
-        {ok, Data} ->
-            process_file(File, Locations,
-                    process_line(Data, CurLoc),
-                    LocNames);
-        eof ->
-            {Locations, lists:reverse(LocNames)}
+        {ok, Line} -> read_line(File, [Line | Acc]);
+        eof -> lists:reverse(Acc)
     end.
 
-location(<<"\"", LocBin/binary>>) ->
-    "\n{ \"" ++ Location = lists:reverse(binary_to_list(LocBin)),
-    list_to_binary(lists:reverse(Location)).
+parse_line([], Zones) -> Zones;
+parse_line([<<"acl \"", Rest/binary>> | T], Zones) ->
+    L = byte_size(Rest) - 4,
+    <<Zone:L/binary, "\" {\n">> = Rest,
+    parse_line(T, [{Zone, <<>>} | Zones]);
+parse_line([<<"};\n">> | T], [{Zone, IPsBin} | Zones]) ->
+    parse_line(T, [{Zone, parse_ips(IPsBin)} | Zones]);
+parse_line([Line | T], [{Zone, IPs} | Zones]) when is_binary(IPs) ->
+    parse_line(T, [{Zone, <<IPs/binary, Line/binary>>} | Zones]);
+parse_line([_ | T], Zones) ->
+    parse_line(T, Zones).
 
-process_line(<<>>, Acc) -> Acc;
-process_line(<<"\n">>, Acc) -> Acc;
-process_line(<<IP:5/binary, ".0/24;", Rest/binary>>, Acc) ->
-    reduce_ips(IP, Acc, Rest);
-process_line(<<IP:6/binary, ".0/24;", Rest/binary>>, Acc) ->
-    reduce_ips(IP, Acc, Rest);
-process_line(<<IP:7/binary, ".0/24;", Rest/binary>>, Acc) ->
-    reduce_ips(IP, Acc, Rest);
-process_line(<<IP:8/binary, ".0/24;", Rest/binary>>, Acc) ->
-    reduce_ips(IP, Acc, Rest);
-process_line(<<IP:9/binary, ".0/24;", Rest/binary>>, Acc) ->
-    reduce_ips(IP, Acc, Rest);
-process_line(<<IP:10/binary, ".0/24;", Rest/binary>>, Acc) ->
-    reduce_ips(IP, Acc, Rest);
-process_line(<<IP:11/binary, ".0/24;", Rest/binary>>, Acc) ->
-    reduce_ips(IP, Acc, Rest).
+%% NOTE: We sort IPs here in order to be able to build Zone -> IP index
+%%       easier in the future. Also, it will speed up the entire IPs list
+%%       sorting.
+parse_ips(IPsBin) ->
+    reduce_ips(
+        lists:sort(
+            split_ips(IPsBin, [])
+        ),
+        []
+    ).
 
-reduce_ips(IP, Acc, Rest) ->
-    {A, B, C} = parse_ip(IP),
-    NewAcc = case Acc of
-        [] ->
-            [{A, B, {C, C}}];
-        [LastIP | T] ->
-            case LastIP of
-                {A, B, {S, E}} when E == C - 1 ->
-                    [{A, B, {S, C}} | T];
-                {_, _, {_, _}} ->
-                    [{A, B, {C, C}}, LastIP | T]
-            end
-    end,
-    process_line(Rest, NewAcc).
+split_ips(<<>>, Acc) -> Acc;
+split_ips(<<"\n", Rest/binary>>, Acc) ->
+    split_ips(Rest, Acc);
+split_ips(<<IP:5/binary, ".0/24;", Rest/binary>>, Acc) ->
+    split_ips(Rest, [parse_ip(IP) | Acc]);
+split_ips(<<IP:6/binary, ".0/24;", Rest/binary>>, Acc) ->
+    split_ips(Rest, [parse_ip(IP) | Acc]);
+split_ips(<<IP:7/binary, ".0/24;", Rest/binary>>, Acc) ->
+    split_ips(Rest, [parse_ip(IP) | Acc]);
+split_ips(<<IP:8/binary, ".0/24;", Rest/binary>>, Acc) ->
+    split_ips(Rest, [parse_ip(IP) | Acc]);
+split_ips(<<IP:9/binary, ".0/24;", Rest/binary>>, Acc) ->
+    split_ips(Rest, [parse_ip(IP) | Acc]);
+split_ips(<<IP:10/binary, ".0/24;", Rest/binary>>, Acc) ->
+    split_ips(Rest, [parse_ip(IP) | Acc]);
+split_ips(<<IP:11/binary, ".0/24;", Rest/binary>>, Acc) ->
+    split_ips(Rest, [parse_ip(IP) | Acc]).
 
 parse_ip(IP) ->
-    list_to_tuple(lists:map(fun list_to_integer/1, string:tokens(binary_to_list(IP), "."))).
+    [A, B, C] = lists:map(
+            fun list_to_integer/1,
+            string:tokens(binary_to_list(IP), ".")
+        ),
+    <<A:8,B:8,C:8>>.
 
-ips_to_binary(IPsGroups) ->
-    BinGroups = lists:map(fun ips_group_to_binary/1, IPsGroups),
-    {WholeBin, AllOffsets} = lists:foldl(
-            fun(BinGroup, {Bin, Offsets}) ->
-                {
-                    <<Bin/binary, BinGroup/binary>>,
-                    <<(byte_size(Bin)):20, Offsets/binary-unit:20>>
-                }
-            end,
-            {<<>>, <<>>}, BinGroups),
-    {WholeBin, <<(byte_size(WholeBin)):20, AllOffsets/binary-unit:20>>}.
+reduce_ips([], Groups) -> Groups;
+reduce_ips([<<A:8,B:8,C:8>> | IPs], [<<A:8,B:8,Start:8,End:8>> | Groups])
+                                                        when End =:= C - 1 ->
+    reduce_ips(IPs, [<<A:8,B:8,Start:8,C:8>> | Groups]);
+reduce_ips([<<A:8,B:8,C:8>> | IPs], Groups) ->
+    reduce_ips(IPs, [<<A:8,B:8,C:8,C:8>> | Groups]).
 
-ips_group_to_binary(IPs) ->
-    lists:foldl(
-            fun({A, B, {S, E}}, Acc) ->
-                <<Acc/binary, A:8, B:8, S:8, E:8>>
-            end,
-            <<>>, IPs).
+zones_index(IPsByZones) ->
+    ZonesList = [Zone || {Zone, _IPs} <- IPsByZones],
+    {ZonesTree, _} = lists:foldl(
+                        fun (Zone, {Acc, Num}) ->
+                            {gb_trees:insert(Zone, Num, Acc), Num + 1}
+                        end,
+                    {gb_trees:empty(), 1}, ZonesList),
+    {
+        list_to_tuple(ZonesList),
+        ZonesTree
+    }.
 
-ip_by_location(IPs, Positions, LocsTree, Loc) when is_list(Loc) ->
-    ip_by_location(IPs, Positions, LocsTree, list_to_binary(Loc));
-ip_by_location(IPs, Positions, LocsTree, Loc) ->
-    {value, Offset} = gb_trees:lookup(Loc, LocsTree),
-    <<_:Offset/binary-unit:20, Start:20, End:20, _/binary-unit:20>> = Positions,
-    ip_ranges(binary_part(IPs, {Start, End - Start}), []).
+extract_ips(IPsByZone, ZonesTree) ->
+    lists:sort(lists:flatten([
+        begin
+            {value, Num} = gb_trees:lookup(Zone, ZonesTree),
+            [{IP, Num} || IP <- IPs]
+        end
+            || {Zone, IPs} <- IPsByZone
+    ])).
 
-ip_ranges(<<>>, IPs) -> IPs;
-ip_ranges(<<A:8, B:8, S:8, E:8, Rest/binary>>, IPs) ->
-    ip_ranges(Rest, [{A, B, {S, E}} | IPs]).
+ips_index(IPZoneKV) ->
+    IPGroups = group_ips(IPZoneKV, []),
+    ZeroIPsIndex =  list_to_binary(
+        lists:duplicate(256 * 256, <<0:32/integer>>)
+    ),
+    merge_ip_groups(IPGroups, <<>>, ZeroIPsIndex).
+
+group_ips([], Groups) -> Groups;
+group_ips([{<<Addr:16/integer, Start:8, End:8>>, Zone} | IPs],
+            [<<Addr:16/integer, Rest/binary>> | Groups]) ->
+    group_ips(IPs, [<<Addr:16/integer, Rest/binary, Start:8, End:8, Zone:16/integer>> | Groups]);
+group_ips([{<<Addr:16/integer, Start:8, End:8>>, Zone} | IPs], Groups) ->
+    group_ips(IPs, [<<Addr:16/integer, Start:8, End:8, Zone:16/integer>> | Groups]).
+
+merge_ip_groups([], IPsBlob, IPsIndex) -> {IPsBlob, IPsIndex};
+merge_ip_groups([<<Addr:16/integer,Ranges/binary>> | Groups], IPsBlob, IPsIndex) ->
+    L = Addr * 4,
+    <<Prefix:L/binary, 0:32/integer, Suffix/binary>> = IPsIndex,
+    merge_ip_groups(Groups, <<IPsBlob/binary, Ranges/binary>>,
+                <<Prefix/binary, (byte_size(IPsBlob)):16/integer, (byte_size(Ranges)):16/integer, Suffix/binary>>).
+
+%%
+%% Tests
+%%
+-include_lib("eunit/include/eunit.hrl").
+-ifdef(TEST).
+
+ip_parse_test() ->
+    [IP = parse_ip(BinIP) || {IP, BinIP} <- test_ips()],
+    ok.
+
+test_ips() ->
+    [
+        {<<192,168,0>>, <<"192.168.0">>},
+        {<<200,23,11>>, <<"200.23.11">>},
+        {<<127,127,127>>, <<"127.127.127">>}
+    ].
+
+ips_parse_test() ->
+    [
+        <<60, 169, 4, 4>>,
+        <<60, 166, 7, 7>>,
+        <<60, 166, 3, 3>>,
+        <<60, 166, 0, 0>>
+    ] =
+        parse_ips(
+            <<"60.166.0.0/24;60.166.7.0/24;\n60.166.3.0/24;60.169.4.0/24;">>
+        ),
+    ok.
+
+line_parse_test() ->
+    [
+
+        {
+            <<"CNC_AnHui">>,
+            [
+                <<58, 243, 45, 45>>,
+                <<58, 242, 46, 48>>
+            ]
+        },
+        {
+            <<"CT_AnHui">>,
+            [
+                <<60, 166, 10, 13>>,
+                <<60, 166, 0, 3>>
+            ]
+        }
+    ] = parse_line(test_lines(), []),
+    ok.
+
+test_lines() ->
+    [
+        <<"acl \"CT_AnHui\" {\n">>,
+        <<"60.166.0.0/24;60.166.1.0/24;60.166.2.0/24;60.166.3.0/24;\n">>,
+        <<"60.166.10.0/24;60.166.11.0/24;60.166.12.0/24;60.166.13.0/24;\n">>,
+        <<"};\n">>,
+        <<"acl \"CNC_AnHui\" {\n">>,
+        <<"58.243.45.0/24;58.242.46.0/24;58.242.47.0/24;58.242.48.0/24;\n">>,
+        <<"};\n">>
+    ].
+
+-endif.
